756e7bd6bafacf3c67a65e8aac2021af647ca0c5
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-04-14 13:01:12 +0300

    creating tests folder

--- app/main/views.py
+++ app/main/views.py
@@ -1,22 +1,28 @@
-from flask import render_template
-from app import app
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_sources,get_articles
+from ..models import Sources
 
-# Views
-@app.route('/')
+#views
+@main.route('/')
 def index():
+	'''
+	view root page function that returns the index the page and its data
+	'''
+	sources = get_sources('business')
+	sports_sources = get_sources('sports')
+	technology_sources = get_sources('technology')
+	entertainment_sources = get_sources('entertainment')
+	title = "News Highlighter"
 
-    '''
-    View root page function that returns the index page and its data
-    '''
+	return render_template('index.html',title = title, sources = sources,sports_sources = sports_sources,technology_sources = technology_sources,entertainment_sources = entertainment_sources)
 
-    title = 'Home -Welcome to The best News Highlight Website Online'
-    return render_template('index.html',title = title)
+@main.route('/sources/<id>')
+def articles(id):
+	'''
+	view articles page
+	'''
+	articles = get_articles(id)
+	title = f'NH | {id}'
 
-
-@app.route('/news/<news_id>')
-def news(news_id):
-    '''
-    View movie page function that returns the news details page and its data
-    '''
-
-    return render_template('news.html',id = news_id)    
+	return render_template('articles.html',title= title,articles = articles)
